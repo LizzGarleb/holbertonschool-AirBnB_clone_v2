f87cecd0c6d1f08e45e2d726a6d2f2416ea6f1e1
LizzGarleb <redscorpion3108@gmail.com>
2023-04-03 10:45:04 -0400

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,8 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Create a prompt for Hello HBNB"""
+    """ Create a route for home"""
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Create a route of /hbnb"""
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
