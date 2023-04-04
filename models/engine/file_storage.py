76f07ba11d1cc034c53bff7a55372061e3883cc4
LizzGarleb <redscorpion3108@gmail.com>
2023-04-04 10:28:07 -0400

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,3 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-        def close(self):
-            """Method for closing Flask connection"""
-            self.reload()
