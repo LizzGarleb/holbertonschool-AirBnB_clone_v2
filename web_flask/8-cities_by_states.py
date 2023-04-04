6651ba1ce841bb8ea02c26db2a34e6144b76b696
LizzGarleb <redscorpion3108@gmail.com>
2023-04-04 11:14:01 -0400

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -58,5 +58,12 @@ def teardown_appcontext(exception):
     storage.close()
 
 
+@app.route('cities_by_state', strict_slashes=False)
+def cities_by_state():
+    """Display a HTML page"""
+    states_li = storage.all(State).values()
+    return render_template('8-cities_by_state.html', cities=states_li)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
